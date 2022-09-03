f0ef58a0551cc89d711046f2fdbb28352aaa451b
Victor-Onyejide <victor.onyejide@gmail.com>
2022-09-03 15:02:49 -0400

    added the types to the EventListener

--- frontend/src/components/ListTodo.tsx
+++ frontend/src/components/ListTodo.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, FormEvent} from "react";
 import useFetch from 'use-http';
 
 interface List {
@@ -24,8 +24,7 @@ export default function ListTodo ({list, setList}: SetProps) {
     const [edit, setEdit] = useState<boolean>(false);
     const [editId, setEditId] = useState<string>('');
     const [editList, setEditList] = useState<string>('');
-    const [checked, setChecked] = useState<Checked[]>([]);
-    var checker = false
+
 
     
     function handleSubmitEdit(event: React.FormEvent<HTMLFormElement>) {
@@ -39,55 +38,74 @@ export default function ListTodo ({list, setList}: SetProps) {
         if (response.ok) {
           const filter = list.filter((item) => item.id !== deleteId)
           setList(filter)
-    
         }
       }
     
       async function editToDo(id: string) {
         if (editList !== '') {
           const todo = editList
-    
-    
           const updatedTodo: List = await patch(`/list/${id}`, { todo })
     
           if (response.ok) {
             const updateList = list.map((item) => (item.id === updatedTodo.id ? updatedTodo : item))
             setList(updateList)
             setEditList('')
-    
           }
         }
         setEdit(false)
-    
-    
-    
+      }
+
+      function checkCheckBoxs () {
+        var checkboxs  = document.querySelectorAll("input[type=checkbox]") ;
+        // as HTMLInputElement
+        console.log("Chech Boxs");
+        console.log(checkboxs);
+
+        var arr_checkboxs = Array.from(checkboxs as NodeListOf<HTMLInputElement>) ;
+        arr_checkboxs.map((checkbox) => { checkbox.addEventListener('click', () => {
+          console.log("onChange");
+          
+          if (checkbox.checked) {
+            const text = checkbox.parentElement?.parentElement?.querySelector('.editContent') as HTMLElement  ;
+            text.style.display = 'block';
+            console.log("True");
+          } 
+          else {
+            const text = checkbox.parentElement?.parentElement?.querySelector('.editContent') as HTMLElement  ;
+            text.style.display = 'none';
+            console.log("False");
+
+          }
+        })});
+
+        
       }
       useEffect (()=> {
-        console.log("Checked")
-        console.log(checked)
-      }, [checked])
+        checkCheckBoxs ();
+      }, [])
     return (
         <>
 
             <div className='list-content'>
                 {/* {loading && <p>Loading ...</p>} */}
                 {error && <p>{error.message}</p>}
+
                 {list && list.map((item, index) =>
                 <div className='items' key={item.id} id={item.id}>
+
                     <span className="item-wrapper">
-                    <input type='checkbox' className="checkbox" onClick={() =>{ setShowEdit(toggle => !toggle); }  }/>
-                    {
-                        (edit && (editId === item.id)) 
-                        ? 
-                        <form onSubmit={handleSubmitEdit}>
-                        <input type='text' placeholder={item.todo} onChange={(e) => setEditList(e.target.value)} />
-                        </form> : <p>{item.todo}</p>
-                    }
+                      <input type='checkbox' className="checkbox"/>
+                      {
+                          (edit && (editId === item.id)) 
+                          ? 
+                          <form onSubmit={handleSubmitEdit}>
+                          <input type='text' placeholder={item.todo} onChange={(e) => setEditList(e.target.value)} />
+                          </form> : <p>{item.todo}</p>
+                      }
                     </span>
 
-                    {
-                      showEdit 
-                     &&  <div className='editContent'>
+                                        
+                    <div className='editContent'>
                       {edit && (editId === item.id) ? <button onClick={() => editToDo(item.id)}>Edit</button> :
                       <> 
                           <span className='edit' onClick={() => { setEdit(toggle => !toggle); setEditId(item.id) }}>
@@ -98,16 +116,10 @@ export default function ListTodo ({list, setList}: SetProps) {
                           </span>
                       </>
                       }
-                      </div>
-                    }
-                    
-
+                    </div>
                 </div>
                 )}
-
             </div>
-
         </>
     ) 
-    
 }
